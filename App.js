3609195721e74fd2baf80d714f322769045e5f58
vivekramini24 <vivekramini24@gmail.com>
2023-09-05 15:00:57 +0530

    namastereact

---
+++ App.js
@@ -0,0 +1,41 @@
+/*const heading = React.createElement(
+    "h1",
+    {id:"heading"},
+    "Hello world from React!"
+ );**/
+ /*
+ * <div id="parent">
+        <div id="child">
+            <h1>I'm h1 tag</h1>
+            <h2>I'm h2 tag</h2>
+        </div>
+        <div id="child2">
+            <h1>I'm h1 tag</h1>
+            <h2>I'm h2 tag</h2>
+        </div>
+    </div>
+ *
+ * 
+ * 
+ * 
+ * 
+  * 
+  */
+ const parent = React.createElement(
+    "div",
+    {id:"parent"},
+  [  React.createElement( "div",{id:"child"},[
+    React.createElement("h1",{},"I'm h1 tag"),
+    React.createElement("h2",{},"I'm h2 tag")
+]), 
+ React.createElement( "div",{id:"child2"},[
+    React.createElement("h1",{},"I'm h1 tag"),
+    React.createElement("h2",{},"I'm h2 tag")
+])]
+ );
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
